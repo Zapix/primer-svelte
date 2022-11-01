13c0dd3be23210dede0b2e9861bff54a1e47cc50
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2022-11-01 18:43:29 +0400

    Add test utils to project

---
+++ src/lib/Counter.test.js
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom'
+
+import {render, fireEvent, screen} from '@testing-library/svelte';
+
+import Counter from './Counter.svelte';
+
+test('render button', () => {
+  render(Counter);
+
+  const button  = screen.getByRole('button');
+
+  expect(button).toBeInTheDocument();
+});
