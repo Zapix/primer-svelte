5cf08451245a9cfe04b9506d148f31565823985b
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2022-11-22 12:18:40 +0400

    Remove blanked code that has been generated with vi

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,21 @@
-<script>
-  import svelteLogo from "./assets/svelte.svg";
-  import Counter from "./lib/Counter.svelte";
+<script lang="ts">
+  import Flash from "./Flash/Flash.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <h1>Primer-svelte components</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+  <h2>Flash</h2>
 
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
+  <strong>default</strong>
+  <Flash variant="default" title="Default flash" icon={true} />
 
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+  <strong>success</strong>
+  <Flash variant="success" title="Success flash" icon={true} />
+
+  <strong>warning</strong>
+  <Flash variant="warning" title="Warning flash" icon={true} />
+
+  <strong>danger</strong>
+  <Flash variant="danger" title="Danger flash" icon={true} />
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
