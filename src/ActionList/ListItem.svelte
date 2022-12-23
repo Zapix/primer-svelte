291bb5882d361922289fa1bc0db6879c9400327f
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2022-12-23 13:54:53 +0400

    Add area to put selectable component

--- src/ActionList/ListItem.svelte
+++ src/ActionList/ListItem.svelte
@@ -1,20 +1,39 @@
 <script lang="ts">
-  import LiWrapper from "./LiWrapper.svelte";
-  import type { ItemAriaRole, ItemVariant } from "./types";
+  import { getContext } from "svelte";
 
+  import { getSpace } from "../theme";
+  import LiWrapper from "./LiWrapper.svelte";
+  import { ACTION_LIST_VARIANT } from "./constants";
+  import type { ItemAriaRole, ItemVariant, SelectionVariant } from "./types";
+
+  const actionListVariant: SelectionVariant = getContext(ACTION_LIST_VARIANT);
   export let tabIndex = 0;
   export let variant: ItemVariant = "default";
+  export let selected = false;
   export let disabled = false;
   export let ariaRole: ItemAriaRole = "listitem";
 </script>
 
 <LiWrapper {tabIndex} {variant} {disabled} {ariaRole} on:click>
+  {#if actionListVariant !== "none"}
+    <div
+      data-testid="selectable-place"
+      class="selectable"
+      style:--selectableWidth={getSpace(3)}
+      style:--selectableMargin={getSpace(2)}
+    />
+  {/if}
   <div class="title">
     <slot />
   </div>
 </LiWrapper>
 
 <style>
+  .selectable {
+    width: var(--selectableWidth);
+    margin-right: var(--selectableMargin);
+  }
+
   .title {
     flex-grow: 1;
   }
--- src/ActionList/ListItemView.svelte
+++ src/ActionList/ListItemView.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
   // View to hide slot values in props. Uses for storybook and tests
   import ListItem from "./ListItem.svelte";
-  import type { ItemVariant } from "./types";
+  import type { ItemVariant, SelectionVariant } from "./types";
   import ActionList from "./ActionList.svelte";
 
+  export let selectionVariant: SelectionVariant = "none";
   export let title: string;
   export let variant: ItemVariant = "default";
   export let disabled: boolean;
+  export let selected: boolean;
 </script>
 
-<ActionList>
-  <ListItem on:click {disabled} {variant}>
+<ActionList variant={selectionVariant}>
+  <ListItem on:click {disabled} {variant} {selected}>
     {title}
   </ListItem>
 </ActionList>
