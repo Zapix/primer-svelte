2bc7cd8ff4a33516583f955967908ebd83865de0
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2023-01-05 12:59:41 +0400

    Add SampleList example

---
+++ src/ActionList/examples/SampleList.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import ActionList from "../ActionList.svelte";
+  import ListItem from "../ListItem.svelte";
+  import Octicon from "../../Octicon/Octicon.svelte";
+</script>
+
+<ActionList>
+  <ListItem>
+    <Octicon iconType="link" slot="lead" />
+    Zapix/primer-svelete
+  </ListItem>
+  <ListItem>
+    <Octicon iconType="law" slot="lead" />
+    MIT Licence
+  </ListItem>
+  <ListItem>
+    <Octicon iconType="star" slot="lead" />
+    256 stars
+  </ListItem>
+  <ListItem>
+    <Octicon iconType="repo-forked" slot="lead" />
+    2 forks
+  </ListItem>
+  <ListItem variant="danger">
+    <Octicon iconType="alert" slot="lead" />
+    4 vulnerabilities
+  </ListItem>
+</ActionList>
